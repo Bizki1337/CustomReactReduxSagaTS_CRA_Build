d70db4925c183b040e74895cd5b72280898843e9
Соловьёв Николай <bizki@MacBook-Pro-Solovev.local>
2023-07-06 17:40:39 +0300

    init basic modules, configured ts config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className="App">
+			home
+		</div>
+	)
 }
 
-export default App;
+export default App
